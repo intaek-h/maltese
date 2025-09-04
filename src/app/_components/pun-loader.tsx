bfccab2884f4e4f8cdd9c01d08dfcfe4e0d1fcf8
intaekh <ahiou2ahiou@gmail.com>
2025-09-04 12:51:32 +0900

    로딩화면 끝

--- src/app/_components/pun-loader.tsx
+++ src/app/_components/pun-loader.tsx
@@ -4,26 +4,17 @@ import { useAtom } from "jotai";
 
 import { shouldShowPunLoader as shouldShowPunLoaderAtom } from "@/store/pun";
 
-const art_3 = [
-  "         /^-^\\",
-  "        / o o \\",
-  "       /   Y   \\",
-  "       V \\ v / V",
-  "         / - \\",
-  "        /    |",
-  "  (    /     |",
-  "   ===/___) ||",
-].join("\n");
-
 export function PunLoader() {
   const [shouldShowPunLoader] = useAtom(shouldShowPunLoaderAtom);
 
   if (!shouldShowPunLoader) return null;
 
   return (
-    <div className="fixed flex justify-center items-center top-0 left-0 right-0 bottom-0 bg-accent-foreground text-amber-400">
-      <i className="">말장난 말티즈</i>
-      <pre className="whitespace-pre ">{art_3}</pre>
+    <div className="fixed flex justify-center items-center top-0 left-0 right-0 bottom-0 bg-[rgb(76,126,239,0.3)] text-[rgb(76,126,239)]">
+      <div className="flex items-center justify-center w-60 h-60 sm:w-[300px] sm:h-[300px] bg-background">
+        <i>말장난 말티즈</i>
+        <pre className="whitespace-pre ml-4 text-xl">૮･ﻌ･ა</pre>
+      </div>
     </div>
   );
 }
