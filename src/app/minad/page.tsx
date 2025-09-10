2f7720c3eeb8c5febc8a43e4111397cdc2f028cd
intaekh <ahiou2ahiou@gmail.com>
2025-09-11 01:00:23 +0900

    testing

--- src/app/minad/page.tsx
+++ src/app/minad/page.tsx
@@ -1,3 +1,12 @@
+import BackgroundImage from "@public/backgrounds/2.jpg";
+import Image from "next/image";
+
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <div>
+        <Image src={BackgroundImage} width={300} height={300} alt="" />
+      </div>
+    </div>
+  );
 }
