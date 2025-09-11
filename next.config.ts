0927c745e21d0042fdca9b6e567413cb24342ff7
intaekh <ahiou2ahiou@gmail.com>
2025-09-11 18:51:51 +0900

    nextjs 이미지 설정 교체

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("https://clever-ibis-869.convex.cloud" + "/**")],
+    remotePatterns: [new URL(process.env.NEXT_PUBLIC_CONVEX_URL + "/**")],
   },
 };
 
